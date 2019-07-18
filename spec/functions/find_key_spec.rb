8fc6c02992ff7a41c3333138bace9c8b47f9bffc
Jan Vansteenkiste <jan@vstone.eu>
2019-07-18 08:18:35 +0200

    Convert find_key to the modern Ruby functions api
    Includes a couple of bugfixes:
    
    * Using find_key with nested hashes would return too early:
      Only the first nested hash would be taken into consideration and nil
      was always returned after looking into it (and no match was found).
    
    * Using find_key where the value is nil in nested hashes

--- spec/functions/find_key_spec.rb
+++ spec/functions/find_key_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'find_key' do
   it { is_expected.not_to eq(nil) }
-  it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, %r{wrong number of arguments}i) }
+  it { is_expected.to run.with_params.and_raise_error(ArgumentError, %r{expects 2 arguments}i) }
 
-  it { is_expected.to run.with_params(1, 2, 3).and_raise_error(Puppet::ParseError, %r{wrong number of arguments}i) }
-  it { is_expected.to run.with_params(1, '').and_raise_error(Puppet::ParseError, %r{unexpected argument type FixNum}i) }
+  it { is_expected.to run.with_params(1, 2, 3).and_raise_error(ArgumentError, %r{expects 2 arguments}i) }
+  it { is_expected.to run.with_params(1, '').and_raise_error(ArgumentError, %r{expects a Hash value}i) }
 
   it { is_expected.to run.with_params({}, 'foobar').and_return(nil) }
 
@@ -20,6 +20,39 @@ describe 'find_key' do
       is_expected.to run.with_params(
         { 'foo' => { 'bar' => 'nested' } }, 'bar'
       ).and_return('nested')
+    end
+  end
+  describe 'should loop nested hashes' do
+    it do
+      is_expected.to run.with_params(
+        {
+          'foo' => {},
+          'bar' => {
+            'y' => '13',
+            'nest' => {
+              'more' => 'nesting',
+              'woopy' => 'looped'
+            }
+          }
+        },
+        'woopy'
+      ).and_return('looped')
+    end
+  end
+  describe 'finds nil' do
+    it do
+      is_expected.to run.with_params(
+        {
+          'foo' => {},
+          'bar' => {
+            'a' => 'b',
+            'c' => 'notnil',
+            'd' => 'e'
+          },
+          'c' => nil
+        },
+        'c'
+      ).and_return(nil)
     end
   end
 end
